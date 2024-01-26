1c4a71d88d56a6b618088095dc472f291b78eb92
syedbadhusha21@gmail.com <syedbadhusha21@gmail.com>
2024-01-26 11:51:30 +0530

    print capital and area

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ request.send();
 request.onload = function(){
     var res = JSON.parse(request.response);
     for(i=0;i<res.length;i++){
-        var comm = res[i].name
-        console.log(comm.common);
+        // var comm = res[i].name
+        //console.log(comm.common);
+        console.log("Capital : " + res[i].capital + " Area : " + res[i].area)
     }
 }
