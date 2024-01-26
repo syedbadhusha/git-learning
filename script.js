c0cd454cfa0e986d7cb9b1d6ca7ddb4a8305c0cd
syedbadhusha21@gmail.com <syedbadhusha21@gmail.com>
2024-01-26 11:38:55 +0530

    rest countries common name

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    for(i=0;i<res.length;i++){
+        var comm = res[i].name
+        console.log(comm.common);
+    }
+}
